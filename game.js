890576d69494f7d28899bc2cab41f8e2287c2a9c
kulendu <56751963+kulendu@users.noreply.github.com>
2020-03-21 20:05:06 +0530

    Add files via upload
    uploading of the resoruce files

---
+++ game.js
@@ -0,0 +1,80 @@
+alert("OBJECTIVE OF THE SIMON GAME :\n1.press A to begin the game , the Game Selector switch to 1 and the Skill Level switch to 1,2, 3 or 4.\n2. Press the START button. Simon will give the first signal. Repeat the signal by pressing the same color buttons.\n3. Simon will duplicate the first signal and add one. Repeat these two signals by pressing the same color lenses, in order.\n4. Simon will duplicate these first two signals and add one.\n5. Continue playing as long as you can repeat each sequence of sig- nals correctly. After the 5th, 9th and 13th signals in a sequence, Simon automatically speeds up.\n6. If you fail to repeat a sequence exactly, or if you take more than 5 seconds to repeat a signal, Simon responds with a BUZZ sound. This means you've lost, and the sequence of signals ends.\n NOW PRESS ok TO BEGIN")
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
+
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
